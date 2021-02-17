b25e2f72ef5af5f8874564cf0605e33cdad40bb3
Brandon <brandon.berry994@gmail.com>
2021-02-17 18:38:38 -0500

    new folders

---
+++ problemcoverage/PolynomialADT.java
@@ -0,0 +1,12 @@
+
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author 
+ *  @version 
+ */
+public interface PolynomialADT {
+
+}
